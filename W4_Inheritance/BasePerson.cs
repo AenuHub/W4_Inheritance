ca05611b2974404a7e4d29b8a86f078f0f62eb1c
AenuHub <jesuskane92@gmail.com>
2024-10-22 14:03:08 +0300

    Add project files.

---
+++ W4_Inheritance/BasePerson.cs
@@ -0,0 +1,38 @@
+﻿namespace W4_Inheritance
+{
+    public abstract class BasePerson
+    {
+        private string _firstName;
+        private string _lastName;
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // abstract method for overriding in inherited classes
+        public abstract void PrintFullInfo();
+    }
+
+    public class Student : BasePerson
+    {
+        private int _studentId;
+
+        public int StudentId { get; set; }
+
+        public override void PrintFullInfo()
+        {
+            Console.WriteLine($"{FirstName} {LastName}, Student Id: {StudentId}");
+        }
+    }
+
+    public class Teacher : BasePerson
+    {
+        private int _salary;
+
+        public int Salary { get; set; }
+
+        public override void PrintFullInfo()
+        {
+            Console.WriteLine($"{FirstName} {LastName}, Salary: {Salary}");
+        }
+    }
+}
---
+++ W4_Inheritance/Program.cs
@@ -0,0 +1,21 @@
+﻿using W4_Inheritance;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Student student = new Student();
+        student.FirstName = "John";
+        student.LastName = "Wick";
+        student.StudentId = 1111111;
+        student.PrintFullInfo();
+
+        Teacher teacher = new Teacher();
+        teacher.FirstName = "Max";
+        teacher.LastName = "Payne";
+        teacher.Salary = 50000;
+        teacher.PrintFullInfo();
+
+        Console.ReadKey();
+    }
+}
